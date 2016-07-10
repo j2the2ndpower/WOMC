1119a9ff65741002eb538924592de3e1b4b827e3
j2the2ndpower <j2the2ndpower@gmail.com>
2016-07-10 12:32:19 -0500

    yes

--- src/main/java/ninja/pixelnova/womc/Menu.java
+++ src/main/java/ninja/pixelnova/womc/Menu.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
